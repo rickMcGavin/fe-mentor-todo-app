14bb9227c8d9d688ae7826af3df17e56f3488f57
Rick McGavin <richard.mcgavin@gmail.com>
2022-06-24 19:20:39 -0400

    fix: filter buttons cursor pointer

--- src/components/molecules/Filter/styles.ts
+++ src/components/molecules/Filter/styles.ts
@@ -17,6 +17,7 @@ export const FilterButton = styled.button<{ $active: boolean }>`
   background: none;
   color: ${({ $active }) =>
     !$active ? " var(--dark-gray-blue)" : "var(--primary)"};
+  cursor: pointer;
   font-size: 0.75rem;
   font-weight: 700;
   text-transform: capitalize;
